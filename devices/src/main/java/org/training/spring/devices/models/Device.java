93701173ce61e0b192ca8806b4821b8acf17e871
JacopoTrincanato <trincanatoj@gmail.com>
2025-05-09 13:14:16 +0200

    create Device.java model and add id and name fields

--- devices/src/main/java/org/training/spring/devices/models/Device.java
+++ devices/src/main/java/org/training/spring/devices/models/Device.java
@@ -1,5 +1,23 @@
 package org.training.spring.devices.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "devices")
 public class Device {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "name field cannot be blank, empty or null")
+    @Size(min = 1, max = 100, message = "name field cannot be shorter than 1 char and longer than 100 chars")
+    private String name;
 
 }
